99e1d2a8af420d6bc104072c3387d64dca512753
parth <parthib.faruk@gmail.com>
2025-06-01 19:34:09 -0700

    add dash bootstrap components import... note this did not fix mobile scaling

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 from dash import callback_context
 from dash import dash_table
-from dash import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
 
 # Performance optimizations
 import functools
